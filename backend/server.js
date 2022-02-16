c9ef31aec7f301f57d7b9a4c4f2f6e7f3f72855c
mirasrobert <mrmirasrobert@gmail.com>
2022-02-16 17:59:42 +0800

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,14 @@ app.use('/api/upload', require('./routes/uploadRoutes'))
 
 // For Deployment
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req, res) => {
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
   })
 }
 
